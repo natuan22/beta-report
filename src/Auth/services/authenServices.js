fca755d0e6faecf9491b9eee3aa827b5c3c574c1
Tuan Nguyen Anh <tuan.na@beta.com.vn>
2024-04-02 14:09:48 +0700

    fix url

--- src/Auth/services/authenServices.js
+++ src/Auth/services/authenServices.js
@@ -5,11 +5,11 @@ const apiUrl = process.env.REACT_APP_BASE_URL;
 
 export const authenServices = {
     userLogin: (data) => {
-        return https.post(`${apiUrl}api/v1/auth/login`, data)
+        return https.post(`${apiUrl}/api/v1/auth/login`, data)
     },
 
     autoLogin: (token) => {
-        return https.get(`${apiUrl}api/v1/user/info`, {
+        return https.get(`${apiUrl}/api/v1/user/info`, {
             headers: {
                 Authorization: "Bearer " + token
             }
@@ -17,7 +17,7 @@ export const authenServices = {
     },
 
     userLogout: () => {
-        return https.post('api/v1/auth/logout', {
+        return https.post('/api/v1/auth/logout', {
             headers: {
                 Authorization: "Bearer " + Cookies.get('at')
             }
