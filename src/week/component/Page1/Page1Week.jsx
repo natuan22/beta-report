57430dd5cac7a77f19c901d644e20eaba573143a
Tuan <anhtuan222001@gmail.com>
2025-01-08 15:00:14 +0700

    fix size text banner reportWeek

--- src/week/component/Page1/Page1Week.jsx
+++ src/week/component/Page1/Page1Week.jsx
@@ -9,16 +9,16 @@ const Page1Week = () => {
   return (
     <div className="h-[1152px] w-[800px] relative  ">
       <img src={img} alt="img" width={800} height={1125} />
-      <div className="absolute top-0 left-0 translate-x-[127px] translate-y-[488px]">
+      <div className="absolute top-0 left-0 translate-x-[125px] translate-y-[488px]">
         <p
-          className="text-[#fff] font-bold text-[60px] leading-[110px] tracking-wide my-1 font-[Poppins] text-outline"
+          className="text-[#fff] font-bold text-[62px] leading-[110px] tracking-wide my-1 font-[Poppins] text-outline"
         >
           {dateParts[0]} - {dateParts[1]}
         </p>
       </div>
-      <div className="absolute top-0 left-0 translate-x-[130px] translate-y-[490px]">
+      <div className="absolute top-0 left-0 translate-x-[128px] translate-y-[490px]">
         <p
-          className="text-[#f4ae3f] font-bold text-[60px] leading-[110px] tracking-wide my-1 font-[Poppins]"
+          className="text-[#f4ae3f] font-bold text-[62px] leading-[110px] tracking-wide my-1 font-[Poppins]"
           // style={{ textShadow: "3px 3px 10px #00000080" }}
         >
           {dateParts[0]} - {dateParts[1]}
